b07bfcbc749599c475b0efdbbcda90ae1a4901b7
Tinh77 <tinhgiang9898@gmail.com>
2018-10-30 15:21:18 +0700

    play

---
+++ myjs.js
@@ -0,0 +1,27 @@
+ function load() {
+    var MUSIC_API = " https://2-dot-backup-server-002.appspot.com/_api/v2/songs/get-free-songs";
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var jsObject = JSON.parse(http.responseText);
+            var content = '';
+            for (var i = 0; i < jsObject.length; i++) {
+                var videoItem = '<tr>';
+                videoItem +=  '<td>'+ jsObject[i].id +'</td>';
+                videoItem += '<td> <img width="200px" src="' + jsObject[i].thumbnail+ '"> </td>';
+                videoItem += '<td>'+ jsObject[i].name +'</td>';
+                videoItem += '<td> <button type="button" onclick="doSomeThing('+ jsObject[i].link +')">Click Me!</button></td>';
+                videoItem += '</tr>';
+                content += videoItem;
+            }
+            document.getElementById('myMusic').innerHTML = content;
+        }
+    };
+    http.open("GET", MUSIC_API, true);
+    http.send();
+}
+ var musicLink = document.getElementById('musicFrame');
+
+ function doSomeThing(musicLink) {
+     musicLink.src = musicLink;
+ }
